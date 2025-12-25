86e49e82c9b5e27dd837f680fab95bc1409c61c6
jlozich <jose.americo@gmail.com>
2025-12-25 11:51:57 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 // Cole aqui as chaves do seu projeto Supabase (Project Settings -> API)
-window.SUPABASE_URL = "COLE_AQUI_SUPABASE_URL";
-window.SUPABASE_ANON_KEY = "COLE_AQUI_SUPABASE_ANON_KEY";
+window.SUPABASE_URL = "https://umskirpleoddlwoqoywq.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_DalV8eflzBzzuyuDFGnifA__vaxGsGs";
