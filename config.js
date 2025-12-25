501b7af1398360c213d1efb38d48ff69ad5c9744
jlozich <jose.americo@gmail.com>
2025-12-25 20:19:33 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 window.SUPABASE_URL = "https://umskirpleoddlwoqoywq.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_DalV8eflzBzzuyuDFGnif";
+window.SUPABASE_ANON_KEY = "sb_publishable_DalV8eflzBzzuyuDFGnifA__vaxGsGs";
+
