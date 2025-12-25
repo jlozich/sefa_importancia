71776e94f38cd08e614c73434414200747116566
jlozich <jose.americo@gmail.com>
2025-12-25 20:11:48 -0300

    Update Supabase ANON key in config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 window.SUPABASE_URL = "https://umskirpleoddlwoqoywq.supabase.co";
-window.SUPABASE_ANON_KEY = "sb_publishable_DalV8eflzBzzuyuDFGnifA__vaxGsGs";
+window.SUPABASE_ANON_KEY = "sb_publishable_DalV8eflzBzzuyuDFGnif";
